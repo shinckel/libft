df90a3f0998e9a8d3f70dd014278c4714ab3e58e
shinckel <sofiahdias@gmail.com>
2023-05-16 13:36:57 +0100

    feat: add ft_calloc

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: shinckel <shinckel@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 14:30:27 by shinckel          #+#    #+#             */
-/*   Updated: 2022/11/22 13:16:03 by shinckel         ###   ########.fr       */
+/*   Updated: 2023/05/16 13:36:28 by shinckel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,21 +33,12 @@ int	ft_atoi(const char *str)
 	return (res * sign);
 }
 
-/* <stdlib.h> convert a string to an integer;
- * (22)first, solve the space problem (jump it);
- * (24)then, turn the value of sign negative if the found symbol is minus;
- * (26)jump the symbol;
- * (28)when finding characters that correspond to numbers;
- * (30)transform it into integer(*str - '0') and add it to its decimal(res * 10);
- * (30)e.g."1234", 1000 + 200 + 30 + 4;
- */
-
-/*int main(void)
-{
-	int	a;
-	int	b;
-	a = ft_atoi("   +12345");
-	b = ft_atoi("     -896664");
-	printf("a: %d | b: %d\n", a, b);
-	return (0);
-}*/
+// int main(void)
+// {
+// 	int	a;
+// 	int	b;
+// 	a = ft_atoi("   +12345");
+// 	b = ft_atoi("     -896664");
+// 	printf("a: %d | b: %d\n", a, b);
+// 	return (0);
+// }
