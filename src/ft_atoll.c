51a70d55621de184eddc2599ad6da2e3ae0b78ba
shinckel <sofiahdias@gmail.com>
2023-08-05 12:50:16 +0100

    feat: add ft_atoll function

---
+++ src/ft_atoll.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoll.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shinckel <shinckel@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/05 12:23:00 by shinckel          #+#    #+#             */
+/*   Updated: 2023/08/05 12:23:26 by shinckel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h";
+
+long long	ft_atoll(const char *nptr)
+{
+	size_t					i;
+	unsigned long long		nbr;
+	int						signe;
+
+	i = 0;
+	nbr = 0;
+	signe = 1;
+	while (ft_isspace(nptr[i]))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			signe = -1;
+		i++;
+	}
+	while (nptr[i] == '0')
+		i++;
+	while (ft_isdigit(nptr[i]))
+	{
+		nbr = nbr * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (signe * nbr);
+}
